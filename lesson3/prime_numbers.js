cb042fd0b772595a8233690821a32b2279ccd70e
kandiana <dianka95@gmail.com>
2021-04-22 01:05:11 +0300

    hw-3 done

---
+++ lesson3/prime_numbers.js
@@ -0,0 +1,16 @@
+primeNumbers = [2];
+
+function isNumberPrime(n) {
+    var i = 0;
+    while (i < primeNumbers.length && n >= primeNumbers[i] ** 2) {          // вот требуемый в задании цикл while :)
+        if (n % primeNumbers[i] === 0) return 0;
+        i++;
+    }
+    return 1;
+}
+
+for (var i = 3; i < 100; i++) {                               // тут тоже можно было бы while вставить, но, мне кажется, for лучше подходит
+    if (isNumberPrime(i)) primeNumbers.push(i);
+}
+
+alert('Простые числа до 100:\n' + primeNumbers.join(', ') + '.\n\nВсего их ' + primeNumbers.length);
